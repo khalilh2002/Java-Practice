5d9a6333948c3869f10895f7bf8ae2cf9efa2cf8
khalilh2002 <khalil.houssein2002@gmail.com>
2024-02-10 19:45:58 +0100

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String args[]) {
         //on crée un jeu
-        Joueur j1 = new Joueur("ahmed");
-        Joueur j2 = new Joueur("khalil");
+        Joueur j1 = new Joueur(args[0]);
+        Joueur j2 = new Joueur(args[1]);
         //la partie se fait sur 10 tirages
         Jeu game = new Jeu(j1,j2);
         game.jouer(10);
