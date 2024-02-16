cea20280b4393d8daa807a6bbcbf52dbffed7dfe
khalilh2002 <khalil.houssein2002@gmail.com>
2024-02-16 14:58:39 +0100

    clean more

--- bank-account/src/CompteBancaire.java
+++ bank-account/src/CompteBancaire.java
@@ -2,140 +2,146 @@ import java.util.Scanner;
 
 public class CompteBancaire {
 
-    private static Scanner sc = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     private String proprietor;
-    double sold;
-    int numero;
-    static int nombreComptes = 0 ;
+    private double balance;
+    private int accountNumber;
+    private static int numberOfAccounts = 0;
 
-    CompteBancaire(){
-        this.proprietor="None";
-        sold=0;
-        numero = nombreComptes++;
+    CompteBancaire() {
+        this.proprietor = "None";
+        balance = 0;
+        accountNumber = numberOfAccounts++;
     }
 
-
-    CompteBancaire( String proprietor){
-        this.proprietor=proprietor;
-        sold=0;
-        numero = nombreComptes++;
+    CompteBancaire(String proprietor) {
+        this.proprietor = proprietor;
+        balance = 0;
+        accountNumber = numberOfAccounts++;
     }
 
+    public double getBalance() {
+        return this.balance;
+    }
 
-    public double getSold(){
-        return this.sold;
+    public int getAccountNumber() {
+        return this.accountNumber;
     }
-    
-    public int getNumero(){
-        return this.numero;
-    }
-    
+
     public String getProprietor() {
         return this.proprietor;
     }
-    
-    public static int getNombreComptes(){
-        return nombreComptes;
-    }
-    
-    public String afficher(){
-        String s="nom : ";
-        s+=this.proprietor+" ";
-        s+="sold : "+this.sold+" ";
-        s+="num : "+this.numero+" ";
 
-        return s;
+    public static int getNumberOfAccounts() {
+        return numberOfAccounts;
     }
 
-    public void verser(double money){
-        if (money >= 0){
-            this.sold+=money;
+    public String displayAccountInfo() {
+        String info = "Owner: " + this.proprietor + " ";
+        info += "Balance: " + this.balance + " ";
+        info += "Account Number: " + this.accountNumber + " ";
+
+        return info;
+    }
+
+    public void deposit(double amount) {
+        if (amount >= 0) {
+            this.balance += amount;
         }
     }
-    public void verser(){
+
+    public void deposit() {
         int retry = 0;
-        while (retry<3) {
-            System.out.println("give me the money :-D  : ");
-            double money = sc.nextDouble();
-            if (money>=0) {
-                this.sold+=money;
+        while (retry < 3) {
+            System.out.println("Enter the deposit amount: ");
+            double amount = scanner.nextDouble();
+            if (amount >= 0) {
+                this.balance += amount;
                 break;
-            }else {
-                System.out.println("le nombre egal a 0 ou negatife !!!!!");
-            } 
+            } else {
+                System.out.println("Amount should be greater than or equal to 0.");
+            }
             retry++;
         }
-        
     }
 
-    public boolean retirer(){
-       
-        System.out.println("give me the money :-): ");
-        double money = sc.nextDouble();
-        if (money>=0 && this.sold >= money) {
-            this.sold-=money;
+    public boolean withdraw() {
+        System.out.println("Enter the withdrawal amount: ");
+        double amount = scanner.nextDouble();
+        if (amount >= 0 && this.balance >= amount) {
+            this.balance -= amount;
             return true;
-        }else{
-            System.out.println("ce argent est imposible !!!!!!");
-            return false;
-        }   
-    }
-
-    public boolean retirer(double money){
-        if (money>=0 && this.sold >= money) {
-            this.sold-=money;
-            return true;
-        }else{
-            return false;
-        }
-    }
-    public boolean virer(CompteBancaire cpt , double money ){
-        if (money > 0 && this.sold>=money){
-            this.sold-=money;
-            cpt.sold+=money;
-            return true;
-        }else{
-            System.out.println("ce argent est imposible !!!!!!");
+        } else {
+            System.out.println("Withdrawal not possible!");
             return false;
         }
     }
 
-    private char menuCompte(){
-        char choix;
-        System.out.println("----------------(._.) Menu ( *_* )-------------------------");
-        System.out.println("c:------------ Consulter");
-        System.out.println("v:------------ verser");
-        System.out.println("r:------------ retirer");
-        System.out.println("q:------------ quiter");
-        System.out.println("--------------------------------------------");
-        choix = sc.next().charAt(0);
-        
-        return choix;
+    public boolean withdraw(double amount) {
+        if (amount >= 0 && this.balance >= amount) {
+            this.balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
     }
-    public void operation(){
-        char choix ;
+
+    public boolean transfer(CompteBancaire recipient, double amount) {
+        if (amount > 0 && this.balance >= amount) {
+            this.balance -= amount;
+            recipient.balance += amount;
+            return true;
+        } else {
+            System.out.println("Transfer not possible!");
+            return false;
+        }
+    }
+
+    private char accountMenu() {
+        char choice;
+        System.out.println("----------------Account Menu-------------------");
+        System.out.println("c: View Account Information");
+        System.out.println("d: Deposit");
+        System.out.println("w: Withdraw");
+        System.out.println("q: Quit");
+        System.out.println("-----------------------------------------------");
+        choice = scanner.next().charAt(0);
+
+        return choice;
+    }
+
+    public void performOperations() {
+        char choice;
         while (true) {
-           choix = menuCompte();
-            switch (choix) {
+            choice = accountMenu();
+            switch (choice) {
                 case 'c':
-                    System.out.println(this);
+                    System.out.println("______________________________________________________________");
+                    System.out.println(this.displayAccountInfo());
+                    System.out.println("_______________________________________________________________");
                     break;
-                case 'v':
-                    this.verser();
+                case 'd':
+                    System.out.println("____________________________________________________________");
+                    this.deposit();
+                    System.out.println("______________________________________________________________");
                     break;
-                case 'r':
-                    this.retirer();
+                case 'w':
+                    System.out.println("________________________________________________________________");
+                    this.withdraw();
+                    System.out.println("_________________________________________________________________");
+
                     break;
                 case 'q':
                     return;
                 default:
-                    System.out.println("the choise is wrong check again (¬_¬ )");
+                    System.out.println("_________________________________________________________");
+
+                    System.out.println("Invalid choice. Please try again.");
+                    System.out.println("_____________________________________________________________");
+
                     break;
             }
         }
-        
     }
-
-
 }
--- bank-account/src/Main.java
+++ bank-account/src/Main.java
@@ -2,17 +2,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner sc = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        System.out.println("ecrire votre nom");
-        //String nom = sc.nextLine();
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
 
-        CompteBancaire new_compte = new CompteBancaire("khail");
+        CompteBancaire newAccount = new CompteBancaire(name);
 
-        new_compte.operation();
-
-
+        newAccount.performOperations();
     }
 }
